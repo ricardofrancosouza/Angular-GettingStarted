f194ff5409a3d5347733c3e7cf0a373f6fc56768
demolayprata <ricardofrancosouza@hotmail.com>
2020-08-01 15:24:35 -0300

    inclusao das rotas

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
@@ -21,7 +23,14 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path:'prodcts/:id', component:ProductDetailComponent},
+      {path:'welcome', component:WelcomeComponent},
+      {path:'', redirectTo:'welcome', pathMatch:'full'},
+      {path:'**', redirectTo:'welcome', pathMatch:'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
